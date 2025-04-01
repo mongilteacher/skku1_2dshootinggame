67c3f2e25aabc333b3594d9bcaad8c16691e0992
mongilteacher <mongilteacher@gmail.com>
2025-04-01 12:53:55 +0900

    Update UI_TouchBounce.cs

--- Assets/02.Scripts/UI/Common/UI_TouchBounce.cs
+++ Assets/02.Scripts/UI/Common/UI_TouchBounce.cs
@@ -6,6 +6,12 @@ using DG.Tweening;
                                              // 인터페이스
 public class UI_TouchBounce : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    // 추가 기능
+    // - 피봇 설정 기능
+    // - 버튼이 있을 경우 버튼이 활성화 됬을때만
+    // - 이징
+    
+    
     // 마우스를 터치하면 크기를 변경(키우거나 늘리거나)
     public float StartScale = 1f;
     public float EndScale = 1.2f;
@@ -25,7 +31,7 @@ public class UI_TouchBounce : MonoBehaviour, IPointerDownHandler, IPointerUpHand
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        _rectTransform.DOScale(new Vector3(EndScale, EndScale, 1f), Duration).SetUpdate(true);
+        _rectTransform.DOScale(new Vector3(EndScale, EndScale, 1f), Duration).SetUpdate(true).;
     }
 
     public void OnPointerUp(PointerEventData eventData)
